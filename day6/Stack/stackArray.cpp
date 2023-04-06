c819b1e0287df3a4fcd2cc5284f029e8d05e7889
supunnadeera <129364689+supunnadeera@users.noreply.github.com>
2023-04-06 13:56:48 +0530

    Initial commit

---
+++ day6/Stack/stackArray.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+class stack
+{
+private:
+    /* data */
+public:
+    stack(/* args */);
+    ~stack();
+};
+
+stack::stack(/* args */)
+{
+}
+
+stack::~stack()
+{
+}
+
+
+int main(){
+
+}
